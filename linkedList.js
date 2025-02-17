658fe9334c4bee3633f9c186ff09ec3d08748f70
crL96 <190506813+crL96@users.noreply.github.com>
2025-02-17 18:26:02 +0100

    Add removeAt index function

--- linkedList.js
+++ linkedList.js
@@ -13,6 +13,7 @@ export default class LinkedList {
     append(value) {
         if (this.head == null) {
             this.prepend(value);
+            return;
         }
         let currentNode = this.head;
         while (currentNode.nextNode != null) currentNode = currentNode.nextNode;
@@ -107,5 +108,21 @@ export default class LinkedList {
         }
         string = string.concat("null");
         return string;
+    }
+
+    removeAt(index) {
+        if (this.head == null) throw Error("List is empty");
+
+        let currentNode = this.head;
+        let lastNode = null;
+        for (let i = 0; i < index; i++) {
+            if (currentNode == null) throw Error("Index is greater than list size");
+            lastNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+        if (lastNode == null) this.shift();
+        else {
+            lastNode.nextNode = currentNode.nextNode;
+        }
     }
 }
